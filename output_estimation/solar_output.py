1f090203d3f04821a6ee3a0495ffc1eccdfbe77a
niebl <caro.niebl@uni-muenster.de>
2025-08-27 21:38:12 +0200

    feat: refactor into arcp compatible code

--- output_estimation/solar_output.py
+++ output_estimation/solar_output.py
@@ -2,8 +2,6 @@ import math
 import os
 from osgeo import gdal
 import rasterio
-from rasterio.plot import show
-import geopandas as gpd
 
 class Solar_panel:
     def __init__(self, size, lat, lon, efficiency=0.2, tilt=None):
@@ -16,72 +14,64 @@ class Solar_panel:
         else:
             self.tilt = tilt
         return
+    
+    # panel: instance of class Solar_panel
+    # gti: gti of solar panel adjusted to panels tilt. GTI (in kWh / m²) is daily total of yearly average.
+    def yr_power_output(self, gti):
+        # efficiency
+        kWh_out = (gti * 365) * self.efficiency #get single m² efficiency
+        kWh_out = kWh_out * self.size #scale to panel size
 
+        return kWh_out
 
-# panel: instance of class Solar_panel
-# gti: gti of solar panel adjusted to panels tilt. GTI (in kWh / m²) is daily total of yearly average.
-def yr_power_output(panel, gti):
-    # efficiency
-    kWh_out = (gti * 365) * panel.efficiency #get single m² efficiency
-    kWh_out = kWh_out * panel.size #scale to panel size
+    # https://www.researchgate.net/publication/236314649_From_global_horizontal_to_global_tilted_irradiance_How_accurate_are_solar_energy_engineering_predictions_in_practice
+    # based on chapter 2 of cited work
+    def panel_gti(self, ghi, dni, dhi, albedo=0.15):
+        # determine theta (sun incidence, by its zenith)
+        solar_zenith = self.yr_avg_sun_zenith(self.lat)
+        azimuth_diff = 0 #we are not assuming the solar panel to have an azimuth at this point as that would reduce efficiency
+        theta = math.acos(
+            math.cos(math.radians(solar_zenith)) * math.cos(math.radians(self.tilt)) +
+            math.sin(math.radians(solar_zenith)) * math.sin(math.radians(self.tilt)) * math.cos(math.radians(azimuth_diff))
+        ) #TODO: verify if this angle of incidence formula is correct.
 
-    return kWh_out
+        # calculate direct irradiance from the sun
+        direct_tilted = dni * max(0, math.cos(theta))
 
-# https://www.researchgate.net/publication/236314649_From_global_horizontal_to_global_tilted_irradiance_How_accurate_are_solar_energy_engineering_predictions_in_practice
-# based on chapter 2 of cited work
-def custom_gti(panel, ghi, dni, dhi, albedo=0.15):
-    # determine theta (sun incidence, by its zenith)
-    solar_zenith = yr_avg_sun_zenith(panel.lat)
-    azimuth_diff = 0 #we are not assuming the solar panel to have an azimuth at this point as that would reduce efficiency
-    theta = math.acos(
-        math.cos(math.radians(solar_zenith)) * math.cos(math.radians(panel.tilt)) +
-        math.sin(math.radians(solar_zenith)) * math.sin(math.radians(panel.tilt)) * math.cos(math.radians(azimuth_diff))
-    ) #TODO: verify if this angle of incidence formula is correct.
+        # determine transposition factors
+        diffuse_transp_factor = (1 + math.cos(math.radians(self.tilt))) / 2 #this is used to transpose (tilt) DHI to the angle of the panel
+        reflectance_transp_factor = (1 - math.cos(math.radians(self.tilt))) / 2 #same for the ground reflectance
 
-    # calculate direct irradiance from the sun
-    direct_tilted = dni * max(0, math.cos(theta))
+        # calculate indirect irradiance
+        diffuse_tilted = dhi * diffuse_transp_factor
+        reflected_tilted = ghi * albedo * reflectance_transp_factor
 
-    # determine transposition factors
-    diffuse_transp_factor = (1 + math.cos(math.radians(panel.tilt))) / 2 #this is used to transpose (tilt) DHI to the angle of the panel
-    reflectance_transp_factor = (1 - math.cos(math.radians(panel.tilt))) / 2 #same for the ground reflectance
+        gti_custom = direct_tilted + diffuse_tilted + reflected_tilted
+        return gti_custom
 
-    # calculate indirect irradiance
-    diffuse_tilted = dhi * diffuse_transp_factor
-    reflected_tilted = ghi * albedo * reflectance_transp_factor
-
-    gti_custom = direct_tilted + diffuse_tilted + reflected_tilted
-    return gti_custom
-
-def sample_gtif(file, lat, lon):
-    raster = rasterio.open(file)
-    sample = raster.sample([(lon,lat)])
-    value = next(sample)
-    return value[0]
-
-def sample_irradiance(datapath, lat, lon):
-    dni = sample_gtif(os.path.join(datapath, "DNI.tif"), lat, lon)
-    dhi = sample_gtif(os.path.join(datapath, "DIF.tif"), lat, lon)
-    ghi = sample_gtif(os.path.join(datapath, "GHI.tif"), lat, lon)
-    return(dni, dhi, ghi)
-
-# get the yearly average sun zenith
-# to be refined if this approximation is not sufficient
-def yr_avg_sun_zenith(lat):
-    return lat + (23.5 / 2) #add (half of) earths tilt to the latitude
+    # get the yearly average sun zenith
+    # to be refined if this approximation is not sufficient
+    def yr_avg_sun_zenith(self, lat):
+        return lat + (23.5 / 2) #add (half of) earths tilt to the latitude
+    
+    def sample_irradiance(self, lat, lon, dni_name="DNI.tif", dhi_name="DIF.tif", ghi_name="GHI.tif"):
+        # sample points from map
+        map = arcpy.mp.ArcGISProject('CURRENT').listMaps('Map')[0]
+        dni = arcpy.GetCellValue_management(map.listLayers(dhi_name)[0].dataSource, f"{lon} {lat}")
+        dhi = arcpy.GetCellValue_management(map.listLayers(dni_name)[0].dataSource, f"{lon} {lat}")
+        ghi = arcpy.GetCellValue_management(map.listLayers(ghi_name)[0].dataSource, f"{lon} {lat}")
+        # parse results as floats
+        dni = float(dni.getOutput(0))
+        dhi = float(dhi.getOutput(0))
+        ghi = float(ghi.getOutput(0))
+        return(dni, dhi, ghi)
 
 def main():
     panel = Solar_panel(3, 52, 7.5, efficiency=0.2, tilt=30)
-
-    dni, dhi, ghi = sample_irradiance(".", panel.lat, panel.lon)
-    print(dni)
-    print(ghi)
-    print(dhi)
-
-    print(sample_gtif("GTI.tif", panel.lat, panel.lon))
-
-    gti = custom_gti(panel, ghi, dni, dhi)
-    print(gti)
-    print(yr_power_output(panel,gti))
+    dni, dhi, ghi = panel.sample_irradiance(panel.lat, panel.lon)
+    gti = panel.panel_gti(ghi, dni, dhi)
+    
+    panel.yr_power_output(gti)
     return
 
 if __name__ == '__main__':
